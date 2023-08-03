68cece0fa1ea98621ead4d8c09c34a738fa55f33
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-08-03 15:43:27 -0400

    Initial add user controller

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import HelloController from "./controllers/hello-controller.js";
+import UserController from "./users/users-controller.js";
 
 const app = express();
 
 HelloController(app);
+UserController(app);
 
 app.listen(4000);
