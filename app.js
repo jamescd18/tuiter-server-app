bb0c532d81d5ec94bfdeced788b4537adfa982b4
James C-D <51571627+jamescd18@users.noreply.github.com>
2023-08-05 17:28:54 -0400

    Add cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
+
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 HelloController(app);
 UserController(app);
